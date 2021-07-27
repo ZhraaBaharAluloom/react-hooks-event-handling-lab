ea51756c819f5a3b4c382e95b5e96df1d1bf24d9
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-07-27 15:51:32 -0700

    events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe = () => {
+  const handleButton = (focusEvent) => {
+    console.log(focusEvent);
+  };
+
+  return (
+    <div>
+      <button
+        onFocus={() => handleButton("Good!")}
+        onBlur={() => handleButton("Hey! Eyes on me!")}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React from "react";
+
+const Keypad = () => {
+  const handlePassword = (event) => {
+    console.log(event);
+  };
+
+  return (
+    <div>
+      <input
+        type="password"
+        name="password"
+        onChange={() => handlePassword("Entering password...")}
+      />
+    </div>
+  );
+};
+
+export default Keypad;
